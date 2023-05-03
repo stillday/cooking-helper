818a3c9dfae66ea95baa7fda0c34e7f652489ef7
Jan Drenkhahn <jan-dennisd@outlook.at>
2023-05-03 15:59:01 +0200

    work in progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<script>
+<script lang="ts">
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let loadedData = [];
+	async function loadData() {
+		const { data: result } = await data.supabase.from('test').select('*').limit(20);
+		loadedData = result;
+	}
+
+	$: if (data.session) {
+		loadData();
+	}
 </script>
 
 <svelte:head>
@@ -9,24 +22,11 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+ 
+{#if data.session}
+  <p>client-side data fetching with RLS</p>
+  <pre>{JSON.stringify(loadedData, null, 2)}</pre>
+{/if}
 
 <style>
 	section {
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,13 +4,13 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ parent }) => {
   const { supabase, session } = await parent();
-  if (!session) {
-    throw redirect(303, '/');
-  }
+  // if (!session) {
+  //   throw redirect(303, '/');
+  // }
   const { data: tableData } = await supabase.from('test').select('*');
 
   return {
-    user: session.user,
+    user: session?.user,
     tableData
   };
 };
