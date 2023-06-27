7d76ed785ba691e1d6fc62d2c5d44c8be9a45a98
Jan Drenkhahn <jan-dennisd@outlook.at>
2023-06-27 14:15:04 +0200

    optimise recipes

--- src/routes/recipesList/[id]/+page.server.ts
+++ src/routes/recipesList/[id]/+page.server.ts
@@ -1,6 +1,6 @@
 // ../recipesList/[id]/+page.server.ts
 
-import { addPlan, addShopping } from '$lib/server/supabase.js';
+import { addPlan } from '$lib/server/supabase.js';
 
 export const load = async ({ locals, params: {id} }) => {
   const { supabase, session } = locals;
@@ -66,7 +66,97 @@ export const actions = {
         date: date
       }
     ]
+
+    async function addShopping(){
+      // abruf des rezepts, ingredients laden
+      // Abruf der Shopping list datenbank an datum, wo ingredients-ids
+      // erhöhen oder hinzufügen am tag
+      console.log('plan', planData[0]['recipe-id']);
+      try {
+        const { data: recipeData, error: recipeError } = await supabase
+        .from('recipe-ingredients')
+        .select(`
+          id,
+          amount,
+          ingredients (
+            id,
+            name
+          ),
+          units (
+            id,
+            name
+          ),
+          "recipe-id"
+        `)
+        .eq('recipe-id', planData[0]['recipe-id']);
+    
+        if (recipeError) {
+          console.log('Error fetching data', recipeError);
+          throw new Error('Failed to fetch data from the database.');
+        }
+    
+        const { data: shoppingListData, error: shoppingListError } = await supabase
+        .from('shopping-list')
+        .select(`
+          id,
+          date,
+          "ingredient-id",
+          "unit-id",
+          amount
+        `)
+        .eq('date', planData[0].date)
+        .in('ingredient-id', recipeData.map(ing => ing.id))
+          
+        if(shoppingListError) {
+          console.log('Error fetching data', shoppingListError);
+          throw new Error('Failed to fetch data from the database.');
+        }
+    
+        let shoppingListUpsert = recipeData.reduce((acc, ingredient) => {
+          console.log('ingred', ingredient);
+          const id = ingredient.id;
+          const accIndex = acc.findIndex(i => i.id === id)
+          console.log(ingredient);
+          if (accIndex > -1) {
+            acc[accIndex].amount += ingredient.amount;
+          } else {
+            acc.push({
+              id,
+              date: planData[0].date,
+              "ingredient-id": ingredient.ingredients.id,
+              "unit-id": ingredient.units.id,
+              amount: ingredient.amount,
+            })
+          }
+          
+          return acc
+        }, shoppingListData);
+        
+        const { data, error } = await supabase.from('shopping-list')
+         .insert(shoppingListUpsert);
+  
+        if (error) {
+          console.error('Failed to save recipe:', error.message);
+          return {
+            status: 500,
+            body: JSON.stringify({ message: 'Failed to save recipe' })
+          };
+        }
+      
+        console.log('Recipe saved successfully:', data);
+      
+        // return data[0].id;
+    
+      } catch (error) {
+        console.error('Failed to save recipe:', error);
+        return {
+          status: 500,
+          body: JSON.stringify({ message: 'Failed to save recipe' })
+        };
+      }
+    }
+    
     await addPlan(planData, supabase);
-    await addShopping(planData, supabase);
+    await addShopping();
   }
 }
